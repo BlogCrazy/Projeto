955a0649d74ad9c3230b9a2b30e4618708c4738e
HugoRodriguesLima <hugo.rodrigues1611@hotmail.com>
2017-10-24 20:15:45 -0200

    Indexação do Projeto
    - Criado a estrutura de pastas;
    - Iniciado a utilização do BS 4.0;
    - Next: Criar o Menu kkkk

--- index.php
+++ index.php
@@ -1,1 +1,19 @@
-Teste
+<!DOCTYPE html>
+<html lang="pt-BR">
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, shink-to-fit=no">
+        <meta charset="UTF-8">
+        <title>BLook</title>
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+    </head>
+    <body>
+        <header>
+            
+        </header>
+        <?php
+        // put your code here
+        ?>
+        <script src="http://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha256-k2WSCIexGzOj3Euiig+TlR8gA0EmPjuc79OEeY5L45g=" crossorigin="anonymous"></script>
+        <script src="js/bootstrap.min.js"></script>
+    </body>
+</html>
